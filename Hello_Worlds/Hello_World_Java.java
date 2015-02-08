9d23dc96b8c3cc55d3273741e2bb0d6946befc9a
David Noftsier <davidnoftsier@yahoo.com>
2015-02-07 20:21:16 -0500

    Fixed hello world java

--- Hello_Worlds/Hello_World_Java.java
+++ Hello_Worlds/Hello_World_Java.java
@@ -1,8 +1,10 @@
 /*  Hello World in Java */
 
-public static void main(String[] args){
-  
-  System.out.println("Hello World!");
-  
-}
+public class Hello_World_Java{
 
+ public static void main(String[] args){
+  
+   System.out.println("Hello World!");
+  
+    }
+}
